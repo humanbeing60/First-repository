928521171ce489293ac575b1f604827cc9b5977e
dac <dac@students.acts.in>
2024-10-17 08:46:19 +0530

    smallest and largest sum program version 1 done

---
+++ SmallestAndLargestSum.java
@@ -0,0 +1,26 @@
+// write a Java program that accepts 3 numbers from the user and finds the sum of the smallest and the largest.
+import java.util.Scanner;
+
+public class SmallestAndLargestSum{
+
+	public static void main(){
+		
+		try(Scanner sc = new Scanner( System.in ) ){
+
+			System.out.println("Enter first number: ");
+			int firstNo = sc.nextInt();
+
+			System.out.println("Enter second number: ");
+			int secondNo = sc.nextInt();
+	
+			System.out.println("Enter third number: ");
+			int thirdNo = sc.nextInt();
+
+			int smallest
+	
+			int sum = smallest + largest;
+			System.out.println( "The sum of smalest and largest number is:" + sum );
+		
+		}
+	}
+}
